1c23d06c901ad4960d7b790fff6594e067a2c70a
Pyae (John) Kyaw <jjpyae@gmail.com>
2023-10-26 20:52:37 -0700

    testing a

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import { keyboard } from "@testing-library/user-event/dist/keyboard";
+
 function App() {
-  return (
-    <h1>Hello from App</h1>
-  );
+  return <h1>Hello from App</h1>;
 }
+keyboard;
 
 export default App;
